978aca1c578ea1a1720fedc2412760de3995c490
binayak <bsilwal@ibuildings.com>
2012-07-05 17:41:03 +0200

    Update master

--- hillarys/selenium/currencyChangeAsFlagIconIsClicked.php
+++ hillarys/selenium/currencyChangeAsFlagIconIsClicked.php
@@ -16,17 +16,17 @@ class currencyChangeAsFlagIconIsClicked extends MainTest
         $this->deleteAllVisibleCookies();
         $this->openAndWait("/");
         
-        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
+        $this->assertEquals("http://dgital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
         $this->assertNotCookie("store");
         $this->clickAndWait("ireland-flag");
         
-        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_euro.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
+        $this->assertEquals("http://digital.com/media//product_strip_wood_euro.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
         
         //$this->deleteCookie("store");
         
         $this->deleteAllVisibleCookies();
         $this->openAndWait("/");
-        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
+        $this->assertEquals("http://digital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
         
         $this->deleteAllVisibleCookies();
         $this->openAndWait("blinds/perfect-fit/venetians");
