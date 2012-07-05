8266c31cbb10332d600353b9c1e925e3620f49e5
binayak <bsilwal@ibuildings.com>
2012-07-05 17:43:10 +0200

    Update master

--- hillarys/selenium/homepageTest.php
+++ hillarys/selenium/homepageTest.php
@@ -6,24 +6,6 @@ include 'baseDef.php';
 class HomepageTest extends MainTest
 {
     
-//    public function testCurrencyChange()
-//    {
-//        $this->deleteAllVisibleCookies();
-//        $this->openAndWait("/");
-//        
-//        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
-//        
-//        $this->clickAndWait("ireland-flag");
-//        
-//        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_euro.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
-//        
-//        $this->deleteCookie("store");
-//        $this->deleteAllVisibleCookies();
-//        $this->openAndWait("/");
-//        
-//        $this->assertEquals("http://uat.hillarys.sessiondigital.com/media//product_strip_wood_uk.jpg", $this->getAttribute("xpath=//div[@id='home-product-strip']/ul/li[1]/a/img/@src"));
-//        
-//    }
     
     public function testQuickShopFilterTest()
     {
@@ -31,7 +13,7 @@ class HomepageTest extends MainTest
         $this->select("color-1","Blue");
         $this->clickAndWait("xpath=//form[@id='quick-search-1']/fieldset/button");
         
-        $this->assertEquals("Made to Measure Blinds - Window Blinds - Web Blinds", $this->getTitle());
+        $this->assertEquals("Made Blinds", $this->getTitle());
         $this->assertTrue($this->isChecked("category_4"));
         $this->assertTrue($this->isChecked("colour_28"));
         
@@ -49,7 +31,7 @@ class HomepageTest extends MainTest
     {
         $this->openAndWait("/");
         $this->quickShopFilterTest();
-        $this->logIn("bsilwal@ibuildings.com", "password");
+        $this->logIn("bsilwal@dings.com", "password");
         $this->clickAndWait("link=My Account");
         $this->clickAndWait("link=My Saved Measurements");
         $this->testQuickShopFilterTest();
